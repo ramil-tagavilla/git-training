4db56ee690e1f932b72762983feec2403d731f54
ramil-tagavilla <ramil.tagavilla@awsys-i.com>
2025-05-29 16:50:46 +0800

    Added book issuance feature

---
+++ Library.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<String> books = new ArrayList<>();
+	private List<String> issuedBooks = new ArrayList<>();
+
+    public void addBook(String book) {
+        books.add(book);
+        System.out.println("Book added: " + book);
+    }
+
+    public List<String> getBooks() {
+        return books;
+    }
+	
+	public String searchBook(String book) {
+		if (books.contains(book)) {
+			return "Book found: " + book;
+		} else {
+			return "Book not found";
+		}
+	}
+	
+	public String issueBook(String book) {
+		if (books.contains(book) && !issuedBooks.contains(book)) {
+			issuedBooks.add(book);
+			books.remove(book);
+			return "Book issued: " + book;
+		} else {
+			return "Book not available for issuance.";
+		}
+	}
+}
