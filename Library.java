888099dfe62ab06bee27a8f62460cf39375e365e
ramil-tagavilla <ramil.tagavilla@awsys-i.com>
2025-05-29 16:49:18 +0800

    Added search book feature

---
+++ Library.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<String> books = new ArrayList<>();
+
+    public void addBook(String book) {
+        books.add(book);
+        System.out.println("Book added: " + book);
+    }
+
+    public List<String> getBooks() {
+        return books;
+    }
+	
+	public String searchBook(String book) {
+		if (books.contains(book)) {
+			return "Book found: " + book;
+		} else {
+			return "Book not found";
+		}
+	}
+}
--- LibraryMain.java
+++ LibraryMain.java
@@ -1,5 +1,8 @@
 public class LibraryMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Library Management System!");
+        Library library = new Library();
+        library.addBook("The Great Gatsby");
+        System.out.println(library.searchBook("The Great Gatsby"));
+        System.out.println(library.searchBook("1984"));
     }
 }
