8a59cb0f3a221fd9099db5462ebb2e1bca48e98b
ramil-tagavilla <ramil.tagavilla@awsys-i.com>
2025-06-02 15:15:13 +0800

    Added search book feature

--- LibraryMain.java
+++ LibraryMain.java
@@ -2,6 +2,7 @@ public class LibraryMain {
     public static void main(String[] args) {
         Library library = new Library();
         library.addBook("The Great Gatsby");
-        System.out.println("Books in library: " + library.getBooks());
+        System.out.println(library.searchBook("The Great Gatsby"));
+        System.out.println(library.searchBook("1984"));
     }
 }
